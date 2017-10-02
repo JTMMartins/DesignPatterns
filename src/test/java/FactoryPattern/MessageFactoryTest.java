aa9bd1f2b497d61442f1a47813fe392a978454a9
Jose Martins <josetabordamartins@gmail.com>
2017-10-02 18:10:26 +0100

    Added test for Factory Pattern

---
+++ src/test/java/FactoryPattern/MessageFactoryTest.java
@@ -0,0 +1,36 @@
+package FactoryPattern;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageFactoryTest {
+
+    /**
+     * Criamos 3 mensagens (1 de cada tipo) e enviamos a mensagem
+     * para verifyMessage que irá confirmar que a mensagem corresponde
+     * ao tipo solicitado.
+     */
+    @Test
+    public void createEmailMessage() {
+
+        Message emailMessage = new MessageFactory().construct(MessageType.EmailMessage);
+        verifyMessage(emailMessage, Email.class);
+
+        Message faxMessage = new MessageFactory().construct(MessageType.FaxMessage);
+        verifyMessage(faxMessage, Fax.class);
+
+        Message smsMessage = new MessageFactory().construct(MessageType.SMSMessage);
+        verifyMessage(smsMessage, SMS.class);
+
+    }
+
+    /**
+     * método que verifica se uma classe é do tipo pretendido
+     */
+    private void verifyMessage(Message message, Class<?> clazz) {
+        assertTrue(clazz.getName(), clazz.isInstance(message));
+        System.out.println("A mensagem pertence a Classe " + clazz.getName());
+    }
+
+}
